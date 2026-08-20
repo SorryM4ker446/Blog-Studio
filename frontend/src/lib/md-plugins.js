5436fd899a18a4dddfa06674618ec000d22d5e3a
SorryM4ker <tmrn0kk4k@qq.com>
2026-08-21 04:04:12 +0800

    refactor: use a typed ESM markdown parser

--- frontend/src/lib/md-plugins.js
+++
@@ -1,9 +0,0 @@
-const { imgSize } = require('@mdit/plugin-img-size');
-
-/**
- * Re-export the modern, browser-safe image size plugin.
- * Supports: ![alt =WxH](url)
- */
-module.exports = {
-  imageSizePlugin: imgSize
-};
